257becf8c8663def235d00f5d22f0a00d40997d8
Cuong Duy Nguyen <103cuong@gmail.com>
2020-07-12 21:34:23 +0700

    fix: 🐳 docker-compose (#1)

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,1 @@
-
 docker-compose -f ../docker-compose-dev.yaml down
--- src/components/config.ts
+++ src/components/config.ts
@@ -1,8 +1,8 @@
 const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 9000,
-  pgHost: process.env.PG_HOST || '127.0.0.1',
-  pgPort: (process.env.PG_PORT as number | undefined) || 5000,
+  pgHost: process.env.PG_HOST || 'db',
+  pgPort: (process.env.PG_PORT as number | undefined) || 5432,
   pgDB: process.env.PG_DB || 'social_network',
   pgUser: process.env.PG_USER || 'postgres',
   pgPassword: process.env.PG_PASSWORD || 'postgres',
